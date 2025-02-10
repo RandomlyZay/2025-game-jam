69b56198ea98b8bc4837380bc809c180a69d08a8
CowJuice19 <131293134+CowJuice19@users.noreply.github.com>
2025-02-10 15:51:13 -0500

    Added interactable System
    Not fully implemented

---
+++ objects/chest.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+@onready var interactable: Area2D = $interactable
+@onready var sprite_2d: Sprite2D = $Sprite2D
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	interactable.interact = _on_interact
+
+
+func _on_interact():
+	if sprite_2d.frame == 0:
+		sprite_2d.fram = 1;
+		interactable.is_interactable = false
+		
