1234efb0e66a5daa1b59609d83e4512c3167f94d
CowJuice19 <131293134+CowJuice19@users.noreply.github.com>
2025-02-13 02:32:43 -0500

    Added Enemy Template

--- objects/interactable.gd
+++ objects/interactable.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 @export var interact_name: String =""
 @export var is_interactable: bool = true
-@export var health: int= 0
+@export var health: int = 0
 
 #callable function
 var interact: Callable = func():
