7f37a33d70dbbd0976231782ee8a48f6bfba6247
Isaiah Haughton <isaiahj.haughton@gmail.com>
2025-02-15 19:00:04 -0500

    Goal Interaction

---
+++ objects/items/goal.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var interactable: Area2D = $interactable
+
+func _ready() -> void:
+	interactable.set_interaction_callable(func(): _on_interact())
+
+func get_interact_name() -> String:
+	return "Enter Next Level"
+	
+func _on_interact() -> void:
+	print("Goal is interacted with")
+	# get_tree().change_scene_to_file("res://stages/level2/level2.tscn")
