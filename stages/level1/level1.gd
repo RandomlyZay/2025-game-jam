8985aa983e263e2360f015da318e30752005132b
Isaiah Haughton <isaiahj.haughton@gmail.com>
2025-02-16 16:26:57 -0500

    Updated Background

--- stages/level1/level1.gd
+++ stages/level1/level1.gd
@@ -2,10 +2,6 @@ extends Node2D
 
 @onready var ui_manager: UIManager
 @onready var dialogue = $HUD/PlayerHUD/DialogueBox
-@onready var camera = $Player/Camera
-@onready var section1: StaticBody2D = $TerrainBounds/Section1
-@onready var section2: StaticBody2D = $TerrainBounds/Section2
-@onready var section3: StaticBody2D = $TerrainBounds/Section3
 
 const SECTION_WIDTH = 1920
 
@@ -29,26 +25,6 @@ func connect_player_signals() -> void:
 		player.player_died.connect(func(): 
 			if is_instance_valid(ui_manager):
 				ui_manager.show_game_over_menu())
-
-func _process(_delta) -> void:
-	if !camera:
-		return
-		
-	var camera_x = camera.get_screen_center_position().x
-	var current_section = floor(camera_x / SECTION_WIDTH)
-	
-	# Position sections based on current section
-	section1.position.x = (current_section - 1) * SECTION_WIDTH
-	section2.position.x = current_section * SECTION_WIDTH
-	section3.position.x = (current_section + 1) * SECTION_WIDTH
-	
-	# Only rotate when we're very close to the end of current section
-	var section_progress = camera_x - (current_section * SECTION_WIDTH)
-	if section_progress > SECTION_WIDTH * 0.95:
-		var temp = section1
-		section1 = section2
-		section2 = section3
-		section3 = temp
 
 func on_dialogue_finished() -> void:
 	get_tree().change_scene_to_file("res://stages/level1/level1.tscn")
